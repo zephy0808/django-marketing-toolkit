06950fe04381049b6883269ecedc223864b41b51
zephy0808 <eugenegarrett.info@gmail.com>
2017-11-01 11:14:26 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-eti-marketing-cms',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='None',
+    description='A simple Django UI for Marketing folks to add landing pages from the admin.',
+    long_description=README,
+    url='https://eti.umn.edu/',
+    author='Mon Sucher',
+    author_email='msucher@umn.edu',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9.4',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: None',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
