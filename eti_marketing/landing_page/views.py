d9cd99c3012a4f8a806b181290cb90ad5e31c56b
zephy0808 <eugenegarrett.info@gmail.com>
2018-01-31 15:16:51 -0600

    Small refactor of landing page detail view

--- eti_marketing/landing_page/views.py
+++ eti_marketing/landing_page/views.py
@@ -5,14 +5,17 @@ from eti_marketing.utils import get_base_template
 
 
 class LandingPageView(DetailView):
+    model = LandingPage
     template_name = 'eti_marketing/landing_page/detail.html'
     context_object_name = 'landing'
 
     def get_queryset(self):
-        if self.request.user.is_superuser:
-            return LandingPage.objects.all()
-        else:
-            return LandingPage.objects.published()
+        queryset = super().get_queryset()
+
+        if not self.request.user.is_superuser:
+            queryset = queryset.published()
+
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
