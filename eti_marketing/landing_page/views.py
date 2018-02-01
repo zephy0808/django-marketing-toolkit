479f49625e330504a10dc2f7a2d488550d5db265
zephy0808 <eugenegarrett.info@gmail.com>
2018-02-01 11:35:43 -0600

    Whoops, merge issues

--- eti_marketing/landing_page/views.py
+++ eti_marketing/landing_page/views.py
@@ -1,10 +1,10 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView as BaseDetailView
 
 from .models import LandingPage
 from eti_marketing.utils import get_base_template
 
 
-class LandingPageView(DetailView):
+class DetailView(BaseDetailView):
     model = LandingPage
     template_name = 'eti_marketing/landing_page/detail.html'
     context_object_name = 'landing'
